5c093ccb55553b4e30c3b9953c151d078f0e1d56
hieubhm-1071 <bui.hai.minh.hieu@sun-asterisk.com>
2021-09-08 14:04:15 +0700

    feat: automatically download weight when initializing predictor

--- stamp_detector/utils.py
+++ stamp_detector/utils.py
@@ -1,5 +1,7 @@
 import torch
+import gdown
 
+DETECTOR_WEIGHT_URL = "https://drive.google.com/uc?id=1M3xY_DkDiA5Eg6Crgko5-rbyAEvo1X4D"
 
 def select_device(device=""):
     cpu = device.lower() == "cpu"
@@ -11,3 +13,7 @@ def load_torch_script_model(weight_path, device="cpu"):
     model = torch.jit.load(weight_path, map_location=device)
     stride = 32
     return model, stride
+
+
+def download_weight(url, output=None, quiet=False):
+    return gdown.download(url=url, output=output, quiet=quiet)
