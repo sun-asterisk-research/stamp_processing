5aba1027e0355d3e2a07924c3dd7df04cd195d33
hieubhm-1071 <bui.hai.minh.hieu@sun-asterisk.com>
2021-09-13 13:55:03 +0700

    Fix import issue with yolov5

--- stamp_processing/__init__.py
+++ stamp_processing/__init__.py
@@ -1,2 +1,7 @@
+import sys
+from stamp_processing.module.yolov5 import YOLO_DIR
+
+sys.path.append(str(YOLO_DIR))
+
 from stamp_processing.detector import StampDetector
 from stamp_processing.remover import StampRemover
