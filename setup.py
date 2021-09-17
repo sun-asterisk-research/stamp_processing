95ce9278ac333b84baf6ccf6777d9077f22b6651
bui.hai.minh.hieu <bui.hai.minh.hieu@sun-asterisk.com>
2021-09-17 10:12:17 +0700

    Add LICENSE and update project config

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ Created on
 @author: Sun* AI Research Team
 """
 
-
-import setuptools
+from setuptools import find_packages, setup
 
 
 with open("README.md", "r", encoding="utf-8") as fh:
@@ -13,41 +12,45 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 INSTALL_REQUIRES = [
     "gdown==3.12.2",
-    "opencv_python==4.1.2.30",
+    "opencv_python>=4.1.2.30",
     "torchvision==0.9.1",
     "torch==1.8.1",
     "fastai==2.3.1",
-    "numpy==1.18.5",
+    "numpy>=1.18.5",
     "PyYAML==5.4.1",
 ]
 
 DISTNAME = "stamp_processing"
 VERSION = "0.0.1"
-LICENSE = ""
-DESCRIPTION = "Stamp detector"
+DESCRIPTION = "Stamp processing package"
 AUTHOR = "Sun* AI Research Team"
-EMAIL = "bui.hai.minh.hieu@sun-asterisk.com"
+EMAIL = "sun.converter.team@gmail.com"
 URL = "https://github.com/sun-asterisk-research/stamp_processing/"
 DOWNLOAD_URL = "https://github.com/sun-asterisk-research/stamp_processing/"
 
 
-setuptools.setup(
+setup(
     name=DISTNAME,
-    version=VERSION,
     author=AUTHOR,
     author_email=EMAIL,
+    use_scm_version={
+        "write_to": "stamp_processing/__version__.py",
+        "version_scheme": "guess-next-dev",
+        "local_scheme": "no-local-version",
+    },
+    setup_requires=["setuptools_scm"],
     description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=URL,
     project_urls={
-        "Bug Tracker": DOWNLOAD_URL,
+        "Bug Tracker": "https://github.com/sun-asterisk-research/stamp_processing/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    packages=setuptools.find_packages(),
+    packages=find_packages(where=".", exclude=["tests"]),
     python_requires=">=3.6",
     zip_safe=False,
     install_requires=INSTALL_REQUIRES,
--- stamp_processing/__init__.py
+++ stamp_processing/__init__.py
@@ -1,7 +1,17 @@
+"""
+    isort: skip_file
+"""
 import sys
 
 from stamp_processing.module.yolov5 import YOLO_DIR
+from pkg_resources import DistributionNotFound, get_distribution
 
+__version__ = None
+try:
+    __version__ = get_distribution("table_reconstruction").version
+except DistributionNotFound:
+    __version__ == "0.0.0"  # package is not installed
+    pass
 
 sys.path.append(str(YOLO_DIR))
 
