889b27059ebea5e11a4995d0419c56d06ae325cf
trungtd-2436 <tran.duc.trung-b@sun-asterisk.com>
2021-09-20 15:58:02 +0700

    test: add more tests

--- stamp_processing/module/yolov5/utils/datasets.py
+++ stamp_processing/module/yolov5/utils/datasets.py
@@ -39,7 +39,5 @@ def letterbox(
         img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
     top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
     left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
-    img = cv2.copyMakeBorder(
-        img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color
-    )  # add border
+    img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
     return img, ratio, (dw, dh)
